d1d2b238a89bff109d57c083c2cfa6426b2088e4
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2016-09-26 09:18:28 -0400

    I think alignmentCount.py is working properly, although I have not yet checked its count of Smith-Waterman alignments.

--- Assignments/I519F16/alignmentCount/alignmentCount.py
+++ Assignments/I519F16/alignmentCount/alignmentCount.py
@@ -7,15 +7,19 @@ import numpy as np
 
 def alignCount_allowDoublegap(n,m) :
     # initial matrix full of zeros
-    matrix = np.zeros([n+1,m+1],dtype=int) 
-    
+    matrix = np.zeros([m+1,n+1],dtype=int) 
+    #print "empty double gap matrix: ", matrix
     # fill first row and column with ones
-    matrix[0,:] = np.ones(m+1)
-    matrix[:,0] = np.ones(n+1)
-
+    matrix[0,:] = np.ones(n+1)
+    matrix[:,0] = np.ones(m+1)
+    #print "ones double gap matrix: ", matrix
     ### Insert your code here
     ### dynamical programming loop to fill the rest of the matrix
-    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            matrix[i,j] = matrix[i-1,j-1] + matrix[i-1,j] + matrix[i,j-1]
+            #print "Filling ", i, ", ", j, " : "
+            #print matrix
 
     # return the right bottom corner
     return matrix[-1][-1]
@@ -26,17 +30,33 @@ def alignCount_allowDoublegap(n,m) :
 
 def alignCount_forbidDoublegap(n,m) :
     # initial matrix full of zeros
-    mat = np.zeros([n+1,m+1],dtype=int) 
-    
+    matrix = np.zeros([m+1,n+1],dtype=int) 
+    #print "no double gaps"
     # fill first row and column with ones
-    mat[0,:] = np.ones(m+1)
-    mat[:,0] = np.ones(n+1)
+    matrix[0,:] = np.ones(n+1)
+    matrix[:,0] = np.ones(m+1)
    
     ### Insert your code here
     ### dynamical programming loop to fill the rest of the matrix
-
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            #print "i= ", i, "  j= ", j
+            horizontal = 0
+            for k in range(1, j):
+                #print "k= ", k
+                horizontal += matrix[i-1, j-1-k]
+                #print "horizontal= ", horizontal
+            
+            vertical = 0
+            for k in range (1, i):
+                #print "k= ", k
+                vertical += matrix[i-1-k, j-1]
+                #print "vertical= ", vertical
+            matrix[i,j] = matrix[i-1,j-1] + horizontal + vertical
+            #print "Filling ", i, ", ", j, " : "
+            #print matrix
     # return the right bottom corner
-    return mat[-1][-1]
+    return matrix[-1][-1]
 
 if __name__ == '__main__' :
 
