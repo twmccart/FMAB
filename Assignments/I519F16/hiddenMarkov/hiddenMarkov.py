c20003ea6832f9d322e63c45ec0a110aa02f28d4
Thomas Wayne McCarthy <mccarthy.tw@gmail.com>
2016-10-14 13:47:43 -0400

    Looks like hiddenMarkov.py is working

--- Assignments/I519F16/hiddenMarkov/hiddenMarkov.py
+++ Assignments/I519F16/hiddenMarkov/hiddenMarkov.py
@@ -18,13 +18,35 @@ def viterbi(initial, transition, emission, observed) :
             ### Insert your code here
             ### Goal : implement forward pass of Viterbi algorithm
             ### Fill the score and trace matrices
+            #print "i, obs= ", i, ", ", obs
+            #print "j, st= ", j, ", ", st
+            
             if i == 0 :
                 ### Fill the first column here
+                score[j,i] = initial[st]*emission[st][obs]
+                trace[j,i] = 0
             else :
                 ### Fill the rest of the columns here
-    
+                #score_choices = []
+                #for k in range(0, len(states)):
+                    #print "i=", i
+                    #print "j=", j
+                    #print "k=", k
+                    #print "score[k,i-1]=", score[k,i-1]
+                    #print "transition[states[k]][st]=", transition[states[k]][st]
+                    #print "emission[st][obs]=", emission[st][obs]
+                    #print "score_choice=", score[k,i-1]*transition[states[k]][st]*emission[st][obs]
+                    #score_choices.append(score[k,j-1]*transition[states[k]][st]*emission[st][obs])
+                #print "Score choices: ", score_choices
+                score[j,i] = max(score[k,i-1]*transition[states[k]][st]*emission[st][obs] for k in range(0, len(states)))
+                ## this is a brilliant use of a lambda function, because argmax() does not work on iterables
+                trace[j,i] = max(range(0, len(states)), key=lambda k: score[k,i-1]*transition[states[k]][st]) ## It is not necessary to include "*emission[st][obs]" because it will be constant for all k
+            #print "score"
+            #print score
+            #print "trace"
+            #print trace
     # trace back
-    z = argmax(score[:,-1])
+    z = argmax(score[:,-1]) #score is an array, not a list
     hidden = states[z]
 
     for i in range(1,len(observed))[::-1] :
